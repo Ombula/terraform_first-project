e59fcd036e1bd2e3367a124fc119b22869f3a096
Ombula <joshuaombula@gmail.com>
2026-03-19 18:55:08 +0300

    Second commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "my_server" {
+  ami                    = "ami-0ec10929233384c7f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World!" > index.html
+            nohup busybox httpd -f -p 8080 & 
+            EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform_example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
