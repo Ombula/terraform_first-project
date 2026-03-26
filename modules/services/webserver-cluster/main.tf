6fd523933a3d5561c3872e2ecb87f9df5b33ae36
Ombula <joshuaombula@gmail.com>
2026-03-26 10:33:56 +0300

    second module release

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -90,18 +90,12 @@ resource "aws_launch_template" "example" {
 
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  # user_data = base64encode(templatefile("${path.module}/user-data.sh", {
-  #   server_port = var.server_port
-  #   db_address  = data.terraform_remote_state.db.outputs.address
-  #   db_port     = data.terraform_remote_state.db.outputs.port
-  #   }))
+  user_data = base64encode(templatefile("${path.module}/user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }))
 
-  user_data = base64encode(<<-EOF
-  #!/bin/bash
-  echo "Hello, World Task 4" > index.html
-  nohup busybox httpd -f -p ${var.server_port} &
-  EOF
-  )
 
   lifecycle {
     create_before_destroy = true
